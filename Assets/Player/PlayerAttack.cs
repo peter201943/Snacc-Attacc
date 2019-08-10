22e60ffad930b5453f1c2d0a58a7898f38a40838
pjm349-drexel <pjm349@drexel.edu>
2019-08-09 20:43:02 -0400

    Ok, here we go: This is the FIRST commit.
    Changes to Unity were as follows:
    Edit > Project Settings > Editor > Asset Serialization Mode: Force Text
    Edit > Project Settings > Editor > Version Control Mode: Visible Meta Files
    
    Changes to Files were as follows:
    Library: DELETED
    Temp: DELETED
    .gitignore: ADDED

---
+++ Assets/Player/PlayerAttack.cs
@@ -0,0 +1,93 @@
+﻿
+//DECLARATIONS
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PLAYERATTACK
+public class PlayerAttack : MonoBehaviour
+{
+
+    //VARIABLES
+    public int Damage = 1;
+    public float FireTime = 0.5f;
+    //Decides if object takes damage
+    public float Range = 50f;
+    //Decides if Player can fire
+    float Timer;
+    Ray ShootRay;
+    RaycastHit ShootHit;
+    //Each Mask decides whether the target can be damaged
+    int ChocolateMask;
+    int CrackerMask;
+    int MarshmellowMask;
+
+    //TEMPVARS
+    public float ExplosionRadius = 50f;
+    public float ExplosionPower = 1000f;
+    Rigidbody PlayerRigidbody;
+
+
+    //AWAKE
+    // Start is called before the first frame update
+    //Assigns masks and effects
+    void Awake()
+    {
+        //Chocolate
+        ChocolateMask = LayerMask.GetMask("Chocolate");
+        //Marshmellow
+        MarshmellowMask = LayerMask.GetMask("Marshmellow");
+        //Cracker
+        CrackerMask = LayerMask.GetMask("Cracker");
+
+
+
+
+        //DELETEME
+        PlayerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    //UPDATE
+    // Update is called once per frame
+    //Decides if player can fire
+    void Update()
+    {
+        //Cool off gun
+        Timer += Time.deltaTime;
+
+        //Check if gun can fire
+        if (Input.GetButton("Fire1") /*&& Timer >= FireTime*/) Fire();
+    }
+
+
+    //FIRE
+    //Lobs a projectile at the enemy
+    void Fire()
+    {
+        //Reset gun cooldown
+        Timer = 0f;
+
+
+
+
+
+
+        //EXPLODE!
+        Vector3 ExplosionPosition = transform.position;
+        Collider[] Targets = Physics.OverlapSphere(ExplosionPosition, ExplosionRadius);
+        foreach (Collider hit in Targets)
+        {
+            Rigidbody HitRigidbody = hit.GetComponent<Rigidbody>();
+            if (HitRigidbody != null)
+            {
+                HitRigidbody.AddExplosionForce(ExplosionPower, ExplosionPosition, ExplosionRadius, 30f);
+            }
+        }
+
+        //EXPLODE DARNIT!
+        PlayerRigidbody.AddExplosionForce(ExplosionPower, transform.position, ExplosionRadius);
+
+        //F******!!!!!!
+
+    }
+}
